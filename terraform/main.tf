c9347eb904102f565cc26e8851371b7589c90844
eastechdemotest <eastechdemotest@gmail.com>
2025-09-17 17:08:04 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,27 @@
+// The provider block no longer contains secrets.
+// It will get them from the environment variables set by Jenkins.
 provider "alicloud" {
-  access_key = "LTAI5t63PHnEB7UjFQ1mrRpP"
-  secret_key = "qzBp5AORnY2eATOHAbK2Edx6rFtziG"
   region = "cn-hongkong"
 }
 
 resource "alicloud_ram_role" "ram_role" {
-  name = "tf-pipeline-test"
+  name        = "tf-pipeline-test"
   description = "test terraform pipeline"
-  document    = <
+  // This is a valid policy document that allows ECS services to assume this role.
+  document    = <<EOF
+{
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ecs.aliyuncs.com"
+        ]
+      }
+    }
+  ],
+  "Version": "1"
+}
+EOF
 }
