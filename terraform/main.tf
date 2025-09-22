16959ef4965ba44dc69e3504e766d4b12c977798
eastechdemotest <eastechdemotest@gmail.com>
2025-09-22 16:18:54 +0800

    s

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,40 @@
-# Provider configuration using RAM user credentials from master account
-provider "alicloud" {
-  region     = var.region
-  alias  = "dev_account"
-
-  assume_role {
-    // This is the ARN of the special, pre-created role in your Dev account.
-    // This is the role the console uses automatically.
-    role_arn     = "acs:ram::5855213964512610:role/ResourceDirectoryAccountAccessRole"
-    
-    session_name = "Jenkins-Terraform-VPC-Creation" // A name for logging
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "~> 1.206.0"
+    }
   }
 }
 
-# Call the VPC module
+# ===== 2. DEFINE THE DEFAULT PROVIDER (for the Master Account) =====
+# This authenticates using the credentials from Jenkins.
+provider "alicloud" {
+  region = "cn-hongkong"
+}
+
+# ===== 3. DEFINE THE ALIASED PROVIDER (for the Dev Account) =====
+# This is the provider configuration that the error says is missing.
+# It assumes the role in the target account.
+provider "alicloud" {
+  alias  = "dev_account"
+  region = "cn-hongkong"
+
+  assume_role {
+    role_arn     = "acs:ram::5855213964512610:role/ResourceDirectoryAccountAccessRole"
+    session_name = "Jenkins-Terraform-VPC-Creation"
+  }
+}
+
+# ===== 4. CALL THE MODULE AND PASS THE PROVIDER =====
+# This calls your VPC module and explicitly gives it the aliased provider.
 module "vpc" {
-    providers = {
+  source = "./module/VPC" # Make sure this path is correct
+
+  providers = {
     alicloud = alicloud.dev_account
   }
-  
-  source         = "./module/VPC"
-  vpc_name       = var.vpc_name
-  vpc_cidr_block = var.vpc_cidr_block
+
+  # Add any variables your module needs here
+  # vpc_name   = "my-development-vpc"
+  # cidr_block = "10.200.0.0/16"
 }
