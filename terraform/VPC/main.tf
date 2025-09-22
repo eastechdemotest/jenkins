a39b257d646465e4b2b2a492c27a83665ad3673e
eastechdemotest <eastechdemotest@gmail.com>
2025-09-22 14:24:02 +0800

    Create main.tf

---
+++ terraform/VPC/main.tf
@@ -0,0 +1,12 @@
+# VPC resource in the dev account
+resource "alicloud_vpc" "vpc" {
+  vpc_name   = var.vpc_name
+  cidr_block = var.vpc_cidr_block
+  description = "VPC for dev account under Core > DEV folder in Hong Kong region"
+
+  tags = {
+    Environment = "dev"
+    Folder      = "Core/DEV"
+    ManagedBy   = "Terraform"
+  }
+}
