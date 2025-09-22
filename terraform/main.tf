962248329cd27dfb457cd5feaebc762a107c6f5c
eastechdemotest <eastechdemotest@gmail.com>
2025-09-22 15:39:29 +0800

    f

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,15 @@
 # Provider configuration using RAM user credentials from master account
 provider "alicloud" {
   region     = var.region
-  account_id = var.dev_account_id
+  alias  = "dev_account"
+
+  assume_role {
+    // This is the ARN of the special, pre-created role in your Dev account.
+    // This is the role the console uses automatically.
+    role_arn     = "acs:ram::5855213964512610:role/ResourceDirectoryAccountAccessRole"
+    
+    session_name = "Jenkins-Terraform-VPC-Creation" // A name for logging
+  }
 }
 
 # Call the VPC module
--- terraform/module/VPC/main.tf
+++ terraform/module/VPC/main.tf
@@ -2,8 +2,9 @@
 resource "alicloud_vpc" "vpc" {
   vpc_name   = var.vpc_name
   cidr_block = var.vpc_cidr_block
+  provider = alicloud.dev_account
   description = "VPC for dev account under Core > DEV folder in Hong Kong region"
-
+  
   tags = {
     Environment = "dev"
     Folder      = "Core/DEV"
