a64761c7b8285e0cbec12d501e0eafa250254a82
eastechdemotest <eastechdemotest@gmail.com>
2025-09-25 17:33:54 +0800

    d

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "vpc_id" {
-  description = "ID of the created VPC"
-  value       = module.vpc.vpc_id
-}
-
 output "application_bucket_name" {
   description = "The name of the newly created OSS application bucket."
   value       = module.oss_bucket.bucket_name
